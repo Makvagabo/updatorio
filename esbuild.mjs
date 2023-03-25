1b76a1832aa5d364468472506f9c186875ed221e
Aleksandr Petropavlovskikh <leizarlok@gmail.com>
2023-03-25 19:15:07 +0300

    feat(*): add releaser, add readme to publish

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,23 +1,29 @@
-import * as esbuild from 'esbuild'
+import * as esbuild from 'esbuild';
 import { copy } from 'esbuild-plugin-copy';
 
 await esbuild.build({
-    entryPoints: ['./src/index.ts'],
-    bundle: true,
-    outfile: './.publish/index.js',
-    target: ['es2020', 'node12'],
-    platform: 'node',
-    packages: 'external',
-    define: {
-        'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`
-    },
-    plugins: [
-        copy({
-            resolveFrom: 'cwd',
-            assets: {
-                from: ['./package.json'],
-                to: ['./.publish/package.json'],
-            },
-        }),
-    ],
+  entryPoints: ['./src/index.ts'],
+  bundle: true,
+  outfile: './.publish/index.js',
+  target: ['es2020', 'node12'],
+  platform: 'node',
+  packages: 'external',
+  define: {
+    'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`,
+  },
+  plugins: [
+    copy({
+      resolveFrom: 'cwd',
+      assets: [
+        {
+          from: ['./package.json'],
+          to: ['./.publish/package.json'],
+        },
+        {
+          from: ['./README.md'],
+          to: ['./.publish/README.md'],
+        },
+      ],
+    }),
+  ],
 });
